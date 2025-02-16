5ec9c6ca531054ff8ea005bcc305a3ac0bb4ae7f
trupti <truptidhone30@gmail.com>
2025-02-16 10:12:49 +0530

    done

--- MyBookEasy/app/src/main/java/com/trupti/mybookeasy/network/RetrofitClient.kt
+++ MyBookEasy/app/src/main/java/com/trupti/mybookeasy/network/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private  const val BASE_URL = "http://192.168.0.102:5000"
+    private  const val BASE_URL = "http://localhost:5000"
 
     val instance:Api by lazy {
         Retrofit.Builder()
